0e5bb84a3a76a24d8c93821c690889439dd8d2d1
Kağan Sarı <kagannsari@gmail.com>
2018-02-10 15:59:02 +0300

    add blocks table datatable configurations and API endpoints

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,57 @@
-$(document).ready(function () {
-  $.get('/api/recentBlocks', function (result) {
-    // console.log(`result: `, result);
-    $('#recentBlocksTable').DataTable({
-      data: result,
-      columns: [
-        { data: 'number' },
-        { data: 'miner' },
-        { data: 'timestamp' },
-        { data: 'txCount' },
-      ],
-    });
+function fillBlocksTable() {
+  $('#recentBlocksTable').DataTable({
+    serverSide: true,
+    ajax: '/api/datatableBlocks',
+    searching: false,
+    order: [0, 'desc'],
+    columns: [
+      // block index
+      {
+        title: 'Block',
+        data: 'number',
+        render: $.fn.dataTable.render.number(',', '.'),
+      }, {
+        title: 'Time',
+        data: 'timestamp',
+        orderable: false,
+        render: function (data) {
+          return (new Date(data * 1000)).toLocaleString();
+        },
+      }, {
+        title: 'TXN',
+        data: 'transactions.length',
+        orderable: false,
+      },
+      {
+        title: 'Miner',
+        data: 'miner',
+        orderable: false,
+      }, {
+        title: 'Gas used',
+        data: 'gasUsed',
+        orderable: false,
+        render: function (data, type, row) {
+          // how much of gas limit is used
+          var percent = (data / row.gasLimit) * 100;
+          // format gasUsed integer
+          var numString = $.fn.dataTable.render.number(',', '.').display(data, type, row);
+          return numString + ' (' + percent.toFixed(2) + '%)';
+        },
+      }, {
+        title: 'Gas limit',
+        data: 'gasLimit',
+        orderable: false,
+        render: $.fn.dataTable.render.number(',', '.'),
+      },
+      // { data: null }, // @TODO: avg gas price
+      // { data: null }, // @TODO: reward
+      {
+        title: 'Uncles',
+        data: 'uncles.length',
+        orderable: false,
+      },
+    ],
   });
-});
+}
+
+$(document).ready(fillBlocksTable);
--- src/server/apiRouter.js
+++ src/server/apiRouter.js
@@ -3,17 +3,32 @@ const express = require('express');
 const router = express.Router();
 const { web3 } = global;
 
-router.get('/recentBlocks', async (req, res) => {
+/**
+ * get all blocks between {start} and {start + length}
+ * @param  {integer} start  first block
+ * @param  {integer} length
+ * @return {array}          blocks in descending order
+ */
+async function getBlocks(start, length) {
   const promises = [];
-  const count = 50;
+  for (let i = start; i <= start + length; i++) {
+    promises.push(web3.eth.getBlock(i));
+  }
+  const blocks = await Promise.all(promises);
+  // first block is the most recent one
+  return blocks.sort((a, b) => b.number - a.number);
+}
+
+/**
+ * returns last {length} blocks
+ * query
+ *  - length (100)
+ */
+router.get('/recentBlocks', async (req, res) => {
+  const length = req.query.length || 100;
   const lastBlockNum = await web3.eth.getBlockNumber();
 
-  for (let i = 0; i <= count; i++) {
-    promises.push(web3.eth.getBlock(lastBlockNum - i));
-  }
-
-  const blocks = await Promise.all(promises);
-  blocks.sort((a, b) => b.number - a.number);
+  const blocks = await getBlocks(lastBlockNum - length, length);
 
   const result = blocks.map(block => ({
     number: block.number,
@@ -22,6 +37,44 @@ router.get('/recentBlocks', async (req, res) => {
     txCount: block.transactions.length,
   }));
   res.send(result);
+});
+
+/**
+ * get appropriate data for datatables library to fill blocks table,
+ * returns latest 25 blocks if no parameters given
+ *
+ * query
+ * - draw   {integer} chunk index (0)
+ * - order  {array}   selected options for sorting, we only use block number at the moment ([{dir:'desc'}])
+ * - start  {integer} starting block number (0)
+ * - length {integer} size of the block chunk (25)
+ */
+router.get('/datatableBlocks', async (req, res) => {
+  const totalBlockNumber = await web3.eth.getBlockNumber();
+
+  const draw = Number(req.query.draw) || 0;
+  const orderDirection = req.query.order[0].dir || 'desc'; // asc|desc
+  let start = Number(req.query.start) || 0;
+  let length = Number(req.query.length) || 25;
+  if (orderDirection === 'desc') start = totalBlockNumber - start - length;
+
+  // trim out-of-range indices
+  if (start < 0) {
+    length += start; // lengh of the last page can be less then default length
+    start = 0;
+  }
+  length = Math.min(length, totalBlockNumber - start);
+
+  const blocks = await getBlocks(start, length);
+  // blocks start from the latest if showing in ascending order
+  if (orderDirection === 'asc') blocks.sort((a, b) => a.number - b.number);
+
+  res.send({
+    draw,
+    recordsTotal: totalBlockNumber,
+    recordsFiltered: totalBlockNumber,
+    data: blocks,
+  });
 });
 
 module.exports = router;
