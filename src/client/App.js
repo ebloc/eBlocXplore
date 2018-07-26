75558c6a6821b4ca6e91af6fdb0e5f2f13817b83
Kağan Sarı <kagannsari@gmail.com>
2018-07-26 13:35:35 +0300

    use local storage for my accounts

--- src/client/App.js
+++ src/client/App.js
@@ -14,21 +14,34 @@ import MyAccounts from './components/MyAccounts';
 
 debug.log = console.log.bind(console); // eslint-disable-line no-console
 
+const getInitialAccountsMap = () => {
+  let accountsMap = localStorage.getItem('accountsMap');
+
+  if (accountsMap) {
+    return JSON.parse(accountsMap);
+  } else {
+    // test accounts
+    accountsMap = {
+      '0x42B085Db58Fd54176CFE935dc52e782A8B36DA05': 'Test 1',
+      '0x3B027ff2D229dD1C7918910dEe32048F5F65b70d': 'Test 2',
+      '0x128c9F368F12C24Cc2a4f88dCDCf3daA13C9667e': 'Test 3'
+    };
+    localStorage.setItem('accountsMap', JSON.stringify(accountsMap));
+    return accountsMap;
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       network: 'ebloc-poa',
-      accountsMap: {
-        '0x42B085Db58Fd54176CFE935dc52e782A8B36DA05': 'Test 1',
-        '0xDEb602db8577Bfc7E02866574339072d06C48396': 'Test 2',
-        '0x3B027ff2D229dD1C7918910dEe32048F5F65b70d': 'Test 3 Test 3 Test 3 Test 3 Test 3 Test 3 Test 3',
-        '0x128c9F368F12C24Cc2a4f88dCDCf3daA13C9667e': 'TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest'
-      }
+      accountsMap: getInitialAccountsMap()
     };
   }
 
   setAccountsMap = (accountsMap) => {
+    localStorage.setItem('accountsMap', JSON.stringify(accountsMap));
     this.setState({
       accountsMap
     });
